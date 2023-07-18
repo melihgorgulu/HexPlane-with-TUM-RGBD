75b39b8d0b98cd5f2cf4a5510f524a043c775b49
Melih <e4hs80@linde.com>
2023-07-18 17:43:07 +0200

    test dataloeders

--- test_tumrgbd_dataloader.py
+++ test_tumrgbd_dataloader.py
@@ -1,12 +1,14 @@
-from hexplane.dataloader.tumrgbd_dataset import TUMRgbdDataset
+#from hexplane.dataloader.tumrgbd_dataset import TUMRgbdDataset
 #from hexplane.dataloader.neural_3D_dataset_NDC import Neural3D_NDC_Dataset
+from hexplane.dataloader.dnerf_dataset import DNerfDataset
 
-data_dir = "/home/lindedigital/projects/HexPlane/data/rgbd_dataset_freiburg3_sitting_static"
-dataset = TUMRgbdDataset(datadir=data_dir)
+#data_dir = "/home/lindedigital/projects/HexPlane/data/rgbd_dataset_freiburg3_sitting_static"
+#dataset = TUMRgbdDataset(datadir=data_dir)
 
 #data_dir = "/home/lindedigital/projects/HexPlane/data/n3dv/flame_salmon_1"
 #dataset = Neural3D_NDC_Dataset(datadir=data_dir)
-breakpoint()
+data_dir = "/home/lindedigital/projects/HexPlane/data/jumpingjacks"
+dataset = DNerfDataset(datadir=data_dir)
 i = 0
 for cur_data in dataset:
     print(i)
