f345cafd1ccbe1fc6d78f44d6ddb2b2291d93dd7
Nicolas Schischka <131670912+nschi00@users.noreply.github.com>
2023-08-05 15:52:32 +0200

    Try out new script on colmap data.

--- hexplane/dataloader/__init__.py
+++ hexplane/dataloader/__init__.py
@@ -1,6 +1,8 @@
 from .dnerf_dataset import DNerfDataset
 from .neural_3D_dataset_NDC import Neural3D_NDC_Dataset
 from .tumrgbd_dataset import TUMRgbdDataset
+from .colmap_dataset import ColmapDataset
+from .your_own_data import YourOwnDataset
 
 def get_train_dataset(cfg, is_stack=False):
     if cfg.data.dataset_name == "dnerf":
@@ -50,6 +52,31 @@ def get_train_dataset(cfg, is_stack=False):
             eval_step=cfg.data.nv3d_ndc_eval_step,
             eval_index=cfg.data.nv3d_ndc_eval_index,
             sphere_scale=cfg.data.nv3d_ndc_sphere_scale,
+        )
+    elif cfg.data.dataset_name == "colmap":
+        train_dataset = ColmapDataset(
+            cfg.data.datadir,
+            "train",
+            cfg.data.downsample,
+            is_stack=is_stack,
+            cal_fine_bbox=cfg.data.cal_fine_bbox,
+            N_vis=cfg.data.N_vis,
+            time_scale=cfg.data.time_scale,
+            scene_bbox_min=cfg.data.scene_bbox_min,
+            scene_bbox_max=cfg.data.scene_bbox_max,
+            N_random_pose=cfg.data.N_random_pose,
+            bd_factor=cfg.data.nv3d_ndc_bd_factor,
+            eval_step=cfg.data.nv3d_ndc_eval_step,
+            eval_index=cfg.data.nv3d_ndc_eval_index,
+            sphere_scale=cfg.data.nv3d_ndc_sphere_scale,
+        )
+    elif cfg.data.dataset_name == "own_data":
+        train_dataset = YourOwnDataset(
+            cfg.data.datadir,
+            "train",
+            cfg.data.downsample,
+            is_stack=is_stack,
+            N_vis=cfg.data.N_vis
         )
     else:
         raise NotImplementedError("No such dataset")
@@ -104,6 +131,31 @@ def get_test_dataset(cfg, is_stack=True):
             eval_step=cfg.data.nv3d_ndc_eval_step,
             eval_index=cfg.data.nv3d_ndc_eval_index,
             sphere_scale=cfg.data.nv3d_ndc_sphere_scale,
+        )
+    elif cfg.data.dataset_name == "colmap":
+        test_dataset = ColmapDataset(
+            cfg.data.datadir,
+            "test",
+            cfg.data.downsample,
+            is_stack=is_stack,
+            cal_fine_bbox=cfg.data.cal_fine_bbox,
+            N_vis=cfg.data.N_vis,
+            time_scale=cfg.data.time_scale,
+            scene_bbox_min=cfg.data.scene_bbox_min,
+            scene_bbox_max=cfg.data.scene_bbox_max,
+            N_random_pose=cfg.data.N_random_pose,
+            bd_factor=cfg.data.nv3d_ndc_bd_factor,
+            eval_step=cfg.data.nv3d_ndc_eval_step,
+            eval_index=cfg.data.nv3d_ndc_eval_index,
+            sphere_scale=cfg.data.nv3d_ndc_sphere_scale,
+        )
+    elif cfg.data.dataset_name == "own_data":
+        test_dataset = YourOwnDataset(
+            cfg.data.datadir,
+            "test",
+            cfg.data.downsample,
+            is_stack=is_stack,
+            N_vis=cfg.data.N_vis,
         )
     else:
         raise NotImplementedError("No such dataset")
